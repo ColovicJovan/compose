55de5c5b8b73073d3bc07672d63febb1726a346e
Jovan Colovic <colovicjovan02@gmail.com>
2025-07-11 15:17:54 +0200

    progress v3

--- src/main/kotlin/net/systemvi/configurator/model/Keymap.kt
+++ src/main/kotlin/net/systemvi/configurator/model/Keymap.kt
@@ -11,11 +11,30 @@ import java.io.FileWriter
 @Serializable
 @optics data class KeyMap(val name:String,val keycaps:List<List<Keycap>>){companion object}
 
-fun KeyMap.setKeyWidth(i:Int,j:Int,width:KeycapWidth): KeyMap=
+fun KeyMap.setKeycapWidth(i:Int, j:Int, width:KeycapWidth): KeyMap=
     KeyMap.keycaps
         .index(i)
         .index(j)
         .set(this,this.keycaps[i][j].overrideWidth(width))
+
+fun KeyMap.setKeycapHeight(i:Int, j:Int, height: KeycapHeight): KeyMap =
+    KeyMap.keycaps
+        .index(i)
+        .index(j)
+        .set(this, this.keycaps[i][j].overrideHeight(height))
+
+fun KeyMap.setKeycapLeftPadding(i:Int, j:Int, padding: KeycapPadding): KeyMap =
+    KeyMap.keycaps
+        .index(i)
+        .index(j)
+        .set(this, this.keycaps[i][j].overrideLeftPadding(padding))
+
+fun KeyMap.setKeycapBottomPadding(i:Int, j:Int, padding: KeycapPadding): KeyMap =
+    KeyMap.keycaps
+        .index(i)
+        .index(j)
+        .set(this, this.keycaps[i][j].overrideBottomPadding(padding))
+
 
 fun KeyMap.updateKeycap(x:Int,y:Int,layer:Int,key:Key): KeyMap=
     KeyMap.keycaps
@@ -24,9 +43,9 @@ fun KeyMap.updateKeycap(x:Int,y:Int,layer:Int,key:Key): KeyMap=
         .layers.index(Index.list(),layer)
         .set(this,key.right())
 
-fun KeyMap.exportStl(name:String){
-    val topPlate= Cube(30.0)
-    val fileWriter= FileWriter(name)
+fun KeyMap.exportStl(name:String) {
+    val topPlate = Cube(30.0)
+    val fileWriter = FileWriter(name)
     fileWriter.write(topPlate.toCSG().toStlString())
     fileWriter.close()
 }
